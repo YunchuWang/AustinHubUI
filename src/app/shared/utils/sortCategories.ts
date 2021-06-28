54c0ac4c23dde334ed6ba0eccdd2a9de579655eb
ssdd2013 <shutao2011@gmail.com>
2021-06-27 23:10:10 -0500

    SQ-13: remove unused doc

--- src/app/shared/utils/sortCategories.ts
+++ src/app/shared/utils/sortCategories.ts
@@ -1,7 +1,3 @@
-/**
- * 转化成RMB元字符串
- * @param digits 当数字类型时，允许指定小数点后数字的个数，默认2位小数
- */
 import { Category } from '../../core/models/Category';
 
 export function sortCategories(a: Category, b: Category): number {
